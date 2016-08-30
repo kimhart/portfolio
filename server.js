a3480349559e769c965702179f5707d3233431c9
Pier 59 Developer <developer@pier59.com>
2016-08-30 18:08:01 -0400

    started design

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
-app.use(methodOverride('_method'));
 
 app.get('/', function (req, res){
   res.render('index');
 })
+
+
+
+
+
+app.listen(process.env.PORT || 9292);
